4a4cfbb40964c545130ea6648f8d66295c987fd5
Olivia Cunningham <Liv.cunninghamgb@gmail.com>
2019-05-08 15:08:13 -0400

    bloody deck made

--- public/main.js
+++ public/main.js
@@ -1,7 +1,37 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+let suits = ['hearts', 'clubs', 'spade', 'diamonds']
+
+let ranks = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'jack',
+  'queen',
+  'king'
+]
+//deck generator: suits + rank//
+let deck = []
+
+//suit generator//
+
+const shuffleDeck = () => {
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < ranks.length; j++) {
+      const suit = suits[i]
+      console.log(suit)
+      const rank = ranks[j]
+      console.log(rank)
+      //Push to deck//
+      deck.push(rank + ' of ' + suit)
+    }
   }
+  console.log(deck)
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.add-button').addEventListener('click', shuffleDeck)
