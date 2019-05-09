5e20b366e7ee1f3156c72bf1b61514acb868e0cb
Olivia Cunningham <Liv.cunninghamgb@gmail.com>
2019-05-09 12:09:54 -0400

    objects created for card deck & restructured indentations

--- public/main.js
+++ public/main.js
@@ -18,35 +18,34 @@ const ranks = [
 // deck generator: suits + rank//
 const deck = []
 
-// suit generator//
-
-const shuffleDeck = () => {
+const createDeck = () => {
   for (let i = 0; i < suits.length; i++) {
+    console.log(suits) // get suit
     for (let j = 0; j < ranks.length; j++) {
-      const suit = suits[i]
-      console.log(suit)
-      const rank = ranks[j]
-      console.log(rank)
+      //get face
       // Push to deck//
-      deck.push(rank + ' of ' + suit)
+      const card = {
+        ranks: ranks[j],
+        suits: suits[i]
+      }
+      deck.push(card)
+      console.log('card pushed to deck')
     }
   }
-  console.log(deck)
 }
-// const randomCard = () => {
-//   document.querySelector(.)
+
+// console.log(deck)
+// console.log(shuffle)
+
+// const shuffle = () => {
+// // move card
+// for (i=deck.length-1; i > 0; i--) {
+// let randomCard = math.floor(math.random() * (i+1))
+// let tempCard = deck[i]
+// deck[i] = deck[randomCard]
+// deck[randomCard] = tempCard
+// }
 // }
 
-const randomPosition = () => {
-  for (let i = deck.length - 1; i >= 0; i -= 1) {
-    const currentCard = deck[i]
-
-for (i < deck.length; i = 0; i++) {
-  // found a random card
-  const randomPosition = Math.floor(Math.random() * i--)
-  // move onto new stack
-  currentCard.push(deck[randomPosition]);
-  deck.splice(randomPosition, 1)
-  }
-}
-document.querySelector('.add-button'), document.addEventListener('click', randomPosition) 
+// document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.shuffle').addEventListener('click', createDeck)
